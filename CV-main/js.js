3e4f40ee196ee727bf1c615ce6c8d8533e4a8395
Maksim <40897895+magihawa@users.noreply.github.com>
2023-12-21 18:51:56 +0300

    Delete CV-main directory

--- CV-main/js.js
+++
@@ -1,8 +0,0 @@
-/* Это объявление переменной кнопки по тегу */
-var button = document.querySelector('button');
-
-/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
-button.addEventListener('click', function() {
-	console.log('log: Выведи этот текст в консоль если кнопка нажмётся');
-	alert('Cause when we descend together, we begin to move as one in perfect unison just like the moon and sun')
-})
