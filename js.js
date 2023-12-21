d856971a17cc53ddab46b58f627e67af3c82ff33
Maksim <40897895+magihawa@users.noreply.github.com>
2023-12-21 18:50:33 +0300

    Simple page

---
+++ js.js
@@ -0,0 +1,47 @@
+// /* Это объявление переменной кнопки по тегу */
+// var button = document.querySelector('button');
+
+// /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+// button.addEventListener('click', function() {
+// 	console.log('log: Выведи этот текст в консоль если кнопка нажмётся');
+// 	alert('Cause when we descend together, we begin to move as one in perfect unison just like the moon and sun')
+// })
+let Mask = false;
+
+function MaskOn(){
+	if(Mask){
+		document.getElementById("name").innerHTML = "Maksim Bauer";
+		document.getElementById("ava").src = "ava_me.jpg";
+	} else {
+		document.getElementById("name").innerHTML = "Booga Booga";
+		document.getElementById("ava").src = "ava2_bart.jpg";
+		MaskOn2();
+	} 
+	Mask = !Mask;
+}
+// ???Пока что не знаю как добавить 3 действие на одну кнопку. Решить что делать
+let Mask2 = false;
+	
+function MaskOn2(){
+	if(Mask2){
+		document.getElementById("name").innerHTML = "Booga Booga";
+		document.getElementById("ava").src = "ava2_bart.jpg";
+	} else {
+		document.getElementById("name").innerHTML = "Ugol'ok";
+		document.getElementById("ava").src = "ava3_son.jpg";
+	}
+	Mask2 = !Mask2;
+};
+
+let isRotated = false;
+
+function rotation(){
+	if (isRotated){
+	document.body.style.transform = "rotate(0deg)";
+	document.body.style.direction = "ltr";
+	} else {
+	document.body.style.transform = "rotate(180deg)";
+	document.body.style.direction = "rtl";
+	}
+	isRotated = !isRotated;
+}
